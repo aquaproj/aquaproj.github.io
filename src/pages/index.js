4570756427c7f4be82084edc6f63277ff1bff589
Suzuki Shunsuke <suzuki.shunsuke.1989@gmail.com>
2021-12-02 22:13:10 +0900

    feat: update

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,6 +11,7 @@ function HomepageHeader() {
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
+        <img src="https://raw.githubusercontent.com/aquaproj/aqua/main/logo/aqua_without_text.svg" alt="aqua Logo" className="top__logo" />
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
