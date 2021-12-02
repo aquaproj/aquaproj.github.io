33e407a456eff3b7a1882fc8734375fc7dda604e
Suzuki Shunsuke <suzuki.shunsuke.1989@gmail.com>
2021-12-02 20:09:59 +0900

    feat: Quick Start

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/tutorial-basics/quick-start">
-            Tutorial
+            Quick Start
           </Link>
         </div>
       </div>
