2e758cf3028cfe789071125520dac675ce105103
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2021-12-21 23:19:51 +0900

    feat: implement the DocSearch

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -111,6 +111,21 @@ const config = {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
       },
+      algolia: {
+        appId: 'OBO5AZ814M',
+        // Public API key: it is safe to commit it
+        apiKey: '7ebd7217e9bd4836c5094b4acdf1a0c9',
+        indexName: 'aquaproj',
+        // Optional: see doc section below
+        // contextualSearch: true,
+        // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+        // externalUrlRegex: 'external\\.com|domain\\.com',
+
+        // Optional: Algolia search parameters
+        searchParameters: {},
+
+        //... other Algolia params
+      },
     }),
 };
 
